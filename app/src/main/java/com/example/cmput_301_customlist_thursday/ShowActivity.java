4679dce6c40507ebe607cbac69f2a249fe20c7ca
“Yile <yile@ualberta.ca>
2023-03-03 09:31:31 -0700

    finish show activity

--- app/src/main/java/com/example/cmput_301_customlist_thursday/ShowActivity.java
+++ app/src/main/java/com/example/cmput_301_customlist_thursday/ShowActivity.java
@@ -2,7 +2,13 @@ package com.example.cmput_301_customlist_thursday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.temporal.ValueRange;
 
 public class ShowActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        String cityName = getIntent().getStringExtra("cityName");
+        Log.d("Value", "cityName is "+cityName);
+        TextView cityText = findViewById(R.id.textView);
+        cityText.setText(cityName);
+        // get the button and add a listener to it
+        Button backButton = findViewById(R.id.button);
+        backButton.setOnClickListener(v -> {
+            // go back to MainActivity
+            finish();
+        });
     }
 }
